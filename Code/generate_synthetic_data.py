79dfe6beceeccde4312863508beaad2c333ca699
Srijita Das <srijitadas2008@gmail.com>
2018-10-17 18:27:10 -0500

    Pushing code for synthetic data

---
+++ Code/generate_synthetic_data.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 17 17:36:17 2018
+
+@author: sxd170431
+"""
+import random as rand
+import numpy as np
+datapath="C:\\Users\\sxd170431\\Desktop\\Work\\Git\\Multiple-Experts\\Data\\"
+data_size=1000
+features=8
+if __name__ == '__main__':
+     
+    
